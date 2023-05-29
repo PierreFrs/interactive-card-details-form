7989400a34860d6809ef1df0b562a27391bd51f1
Pierre <pierrefraisse90@gmail.com>
2023-05-29 15:42:59 +0200

    feat : UI

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      primaryLinearGradient:
+        "linear-gradient(to top, rgba(222,189,255,1), rgba(96,5,148,1))",
+      primaryRed: "hsl(0, 100%, 66%)",
+      neutralWhite: "hsl(0, 0%, 100%)",
+      neutralLightGrayishViolet: "hsl(270, 3%, 87%)",
+      neutralDarkGrayishViolet: "hsl(279, 6%, 55%)",
+      neutralVeryDarkViolet: "hsl(278, 68%, 11%)",
+    },
+    extend: {
+      fontFamily: {
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
